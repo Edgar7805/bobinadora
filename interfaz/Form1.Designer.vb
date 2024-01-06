5f3ee8cfbd4ee30b51dfd021be0eb7cf86c8e981
Edgar7805 <96481681+Edgar7805@users.noreply.github.com>
2024-01-05 23:16:13 -0300

    Add files via upload
    interfaz serial hecha en visual basic

---
+++ interfaz/Form1.Designer.vb
@@ -0,0 +1,249 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Btnevniardatos = New System.Windows.Forms.Button()
+        Me.PuertoCom = New System.IO.Ports.SerialPort(Me.components)
+        Me.TxtVueltas = New System.Windows.Forms.TextBox()
+        Me.Lbrespuesta = New System.Windows.Forms.Label()
+        Me.Cbcalibre = New System.Windows.Forms.ComboBox()
+        Me.TxtLargoBobina = New System.Windows.Forms.TextBox()
+        Me.ImageList1 = New System.Windows.Forms.ImageList(Me.components)
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.Ayuda2ToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LbcalibreAWG = New System.Windows.Forms.Label()
+        Me.Lblargobobina = New System.Windows.Forms.Label()
+        Me.LbVueltas = New System.Windows.Forms.Label()
+        Me.Btnconectar = New System.Windows.Forms.Button()
+        Me.Cbopuertos = New System.Windows.Forms.ComboBox()
+        Me.Btnbuscarpuertos = New System.Windows.Forms.Button()
+        Me.Txtdatosrecibidos = New System.Windows.Forms.TextBox()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Btnevniardatos
+        '
+        Me.Btnevniardatos.Location = New System.Drawing.Point(13, 199)
+        Me.Btnevniardatos.Margin = New System.Windows.Forms.Padding(4)
+        Me.Btnevniardatos.Name = "Btnevniardatos"
+        Me.Btnevniardatos.Size = New System.Drawing.Size(100, 28)
+        Me.Btnevniardatos.TabIndex = 4
+        Me.Btnevniardatos.Text = "enviar datos"
+        Me.Btnevniardatos.UseVisualStyleBackColor = True
+        '
+        'PuertoCom
+        '
+        '
+        'TxtVueltas
+        '
+        Me.TxtVueltas.Location = New System.Drawing.Point(11, 141)
+        Me.TxtVueltas.Margin = New System.Windows.Forms.Padding(4)
+        Me.TxtVueltas.Name = "TxtVueltas"
+        Me.TxtVueltas.Size = New System.Drawing.Size(132, 22)
+        Me.TxtVueltas.TabIndex = 5
+        Me.TxtVueltas.Text = "-Ingresar-"
+        '
+        'Lbrespuesta
+        '
+        Me.Lbrespuesta.AutoSize = True
+        Me.Lbrespuesta.BackColor = System.Drawing.Color.Transparent
+        Me.Lbrespuesta.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Lbrespuesta.Location = New System.Drawing.Point(160, 203)
+        Me.Lbrespuesta.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Lbrespuesta.Name = "Lbrespuesta"
+        Me.Lbrespuesta.Size = New System.Drawing.Size(132, 18)
+        Me.Lbrespuesta.TabIndex = 7
+        Me.Lbrespuesta.Text = "Datos recibidos:"
+        '
+        'Cbcalibre
+        '
+        Me.Cbcalibre.FormattingEnabled = True
+        Me.Cbcalibre.Items.AddRange(New Object() {"26", "30", "43"})
+        Me.Cbcalibre.Location = New System.Drawing.Point(317, 141)
+        Me.Cbcalibre.Margin = New System.Windows.Forms.Padding(4)
+        Me.Cbcalibre.Name = "Cbcalibre"
+        Me.Cbcalibre.Size = New System.Drawing.Size(132, 24)
+        Me.Cbcalibre.TabIndex = 8
+        Me.Cbcalibre.Text = "-Seleccionar-"
+        '
+        'TxtLargoBobina
+        '
+        Me.TxtLargoBobina.Location = New System.Drawing.Point(163, 141)
+        Me.TxtLargoBobina.Margin = New System.Windows.Forms.Padding(4)
+        Me.TxtLargoBobina.Name = "TxtLargoBobina"
+        Me.TxtLargoBobina.Size = New System.Drawing.Size(132, 22)
+        Me.TxtLargoBobina.TabIndex = 11
+        Me.TxtLargoBobina.Text = "-Ingresar-"
+        '
+        'ImageList1
+        '
+        Me.ImageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit
+        Me.ImageList1.ImageSize = New System.Drawing.Size(16, 16)
+        Me.ImageList1.TransparentColor = System.Drawing.Color.Transparent
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.BackColor = System.Drawing.Color.SlateGray
+        Me.MenuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.Ayuda2ToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional
+        Me.MenuStrip1.Size = New System.Drawing.Size(542, 28)
+        Me.MenuStrip1.TabIndex = 16
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'Ayuda2ToolStripMenuItem
+        '
+        Me.Ayuda2ToolStripMenuItem.Name = "Ayuda2ToolStripMenuItem"
+        Me.Ayuda2ToolStripMenuItem.Size = New System.Drawing.Size(65, 24)
+        Me.Ayuda2ToolStripMenuItem.Text = "Ayuda"
+        '
+        'LbcalibreAWG
+        '
+        Me.LbcalibreAWG.AutoSize = True
+        Me.LbcalibreAWG.BackColor = System.Drawing.Color.Transparent
+        Me.LbcalibreAWG.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LbcalibreAWG.Location = New System.Drawing.Point(315, 122)
+        Me.LbcalibreAWG.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.LbcalibreAWG.Name = "LbcalibreAWG"
+        Me.LbcalibreAWG.Size = New System.Drawing.Size(110, 18)
+        Me.LbcalibreAWG.TabIndex = 9
+        Me.LbcalibreAWG.Text = "Calibre AWG:"
+        '
+        'Lblargobobina
+        '
+        Me.Lblargobobina.AutoSize = True
+        Me.Lblargobobina.BackColor = System.Drawing.Color.Transparent
+        Me.Lblargobobina.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Lblargobobina.Location = New System.Drawing.Point(161, 123)
+        Me.Lblargobobina.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Lblargobobina.Name = "Lblargobobina"
+        Me.Lblargobobina.Size = New System.Drawing.Size(134, 18)
+        Me.Lblargobobina.TabIndex = 10
+        Me.Lblargobobina.Text = "Largo de bobina:"
+        '
+        'LbVueltas
+        '
+        Me.LbVueltas.AutoSize = True
+        Me.LbVueltas.BackColor = System.Drawing.Color.Transparent
+        Me.LbVueltas.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LbVueltas.Location = New System.Drawing.Point(8, 119)
+        Me.LbVueltas.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.LbVueltas.Name = "LbVueltas"
+        Me.LbVueltas.Size = New System.Drawing.Size(68, 18)
+        Me.LbVueltas.TabIndex = 12
+        Me.LbVueltas.Text = "Vueltas:"
+        '
+        'Btnconectar
+        '
+        Me.Btnconectar.Location = New System.Drawing.Point(318, 54)
+        Me.Btnconectar.Margin = New System.Windows.Forms.Padding(4)
+        Me.Btnconectar.Name = "Btnconectar"
+        Me.Btnconectar.Size = New System.Drawing.Size(100, 28)
+        Me.Btnconectar.TabIndex = 3
+        Me.Btnconectar.Text = "CONECTAR"
+        Me.Btnconectar.UseVisualStyleBackColor = True
+        '
+        'Cbopuertos
+        '
+        Me.Cbopuertos.FormattingEnabled = True
+        Me.Cbopuertos.Location = New System.Drawing.Point(164, 54)
+        Me.Cbopuertos.Margin = New System.Windows.Forms.Padding(4)
+        Me.Cbopuertos.Name = "Cbopuertos"
+        Me.Cbopuertos.Size = New System.Drawing.Size(119, 24)
+        Me.Cbopuertos.TabIndex = 1
+        Me.Cbopuertos.Text = "-Seleccionar-"
+        '
+        'Btnbuscarpuertos
+        '
+        Me.Btnbuscarpuertos.Location = New System.Drawing.Point(13, 50)
+        Me.Btnbuscarpuertos.Margin = New System.Windows.Forms.Padding(4)
+        Me.Btnbuscarpuertos.Name = "Btnbuscarpuertos"
+        Me.Btnbuscarpuertos.Size = New System.Drawing.Size(100, 28)
+        Me.Btnbuscarpuertos.TabIndex = 2
+        Me.Btnbuscarpuertos.Text = "buscar puertos"
+        Me.Btnbuscarpuertos.UseVisualStyleBackColor = True
+        '
+        'Txtdatosrecibidos
+        '
+        Me.Txtdatosrecibidos.BackColor = System.Drawing.SystemColors.InactiveCaptionText
+        Me.Txtdatosrecibidos.ForeColor = System.Drawing.Color.Lime
+        Me.Txtdatosrecibidos.Location = New System.Drawing.Point(301, 203)
+        Me.Txtdatosrecibidos.Margin = New System.Windows.Forms.Padding(4)
+        Me.Txtdatosrecibidos.Multiline = True
+        Me.Txtdatosrecibidos.Name = "Txtdatosrecibidos"
+        Me.Txtdatosrecibidos.Size = New System.Drawing.Size(146, 19)
+        Me.Txtdatosrecibidos.TabIndex = 6
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(542, 422)
+        Me.Controls.Add(Me.Btnbuscarpuertos)
+        Me.Controls.Add(Me.Btnconectar)
+        Me.Controls.Add(Me.Btnevniardatos)
+        Me.Controls.Add(Me.Cbopuertos)
+        Me.Controls.Add(Me.LbVueltas)
+        Me.Controls.Add(Me.Lblargobobina)
+        Me.Controls.Add(Me.Lbrespuesta)
+        Me.Controls.Add(Me.Txtdatosrecibidos)
+        Me.Controls.Add(Me.LbcalibreAWG)
+        Me.Controls.Add(Me.Cbcalibre)
+        Me.Controls.Add(Me.TxtVueltas)
+        Me.Controls.Add(Me.TxtLargoBobina)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Btnevniardatos As Button
+    Friend WithEvents PuertoCom As IO.Ports.SerialPort
+    Friend WithEvents TxtVueltas As TextBox
+    Friend WithEvents Lbrespuesta As Label
+    Friend WithEvents Cbcalibre As ComboBox
+    Friend WithEvents TxtLargoBobina As TextBox
+    Friend WithEvents ImageList1 As ImageList
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents Ayuda2ToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LbcalibreAWG As Label
+    Friend WithEvents Lblargobobina As Label
+    Friend WithEvents LbVueltas As Label
+    Friend WithEvents Btnconectar As Button
+    Friend WithEvents Cbopuertos As ComboBox
+    Friend WithEvents Btnbuscarpuertos As Button
+    Friend WithEvents Txtdatosrecibidos As TextBox
+End Class
---
+++ interfaz/Form1.vb
@@ -0,0 +1,246 @@
+﻿Imports System.Linq.Expressions
+Imports System.Reflection
+
+
+
+
+
+Public Class Form1
+
+    Dim StrBufferEntrada As String    'guarda datos de entrada
+    Dim StrBufferSalida As String      'guarda datos de salida
+    Dim enviando As Boolean = False  'el estado no recibe datos si es true
+    Dim estado As String
+    Dim TipoPaso As String
+    Dim PasoxVuelta As String
+    Dim LoopCambiarSentido As Decimal
+    Dim DiametroDeCable As Decimal
+
+
+
+
+    Private Delegate Sub DelegadoAcceso(ByVal AdicionarTexto As String)
+
+
+    Private Sub AccesoFormPrincipal(ByVal TextoForm As String)
+        StrBufferEntrada = TextoForm
+
+        ''-------------------------
+        Txtdatosrecibidos.Text = StrBufferEntrada
+
+
+    End Sub
+
+    Private Sub PuertaAccesoInterrupcion(ByVal BufferIn As String)
+        Dim TextoInterrupcion() As Object = {BufferIn}
+        Dim DelegadoInterrupcion As DelegadoAcceso
+        DelegadoInterrupcion = New DelegadoAcceso(AddressOf AccesoFormPrincipal)
+        MyBase.Invoke(DelegadoInterrupcion, TextoInterrupcion)
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'inicio variables
+        estado = "1"
+        StrBufferEntrada = ""
+        StrBufferSalida = ""
+        Btnconectar.Enabled = False
+        Btnevniardatos.Enabled = False
+        TipoPaso = "1"
+        PasoxVuelta = "1"
+        DiametroDeCable = "0.4"
+        Btnevniardatos.Enabled = False
+    End Sub
+
+
+    Private Sub Btnbuscarpuetos_Click(sender As Object, e As EventArgs) Handles Btnbuscarpuertos.Click
+
+
+        Cbopuertos.Items.Clear() 'busca puertos diponibles
+
+        For Each PuertoDiponible As String In My.Computer.Ports.SerialPortNames
+            Cbopuertos.Items.Add(PuertoDiponible)
+        Next
+
+        If Cbopuertos.Items.Count >= 0 Then
+            Cbopuertos.Text = Cbopuertos.Items(0)
+            MessageBox.Show("seleccione puerto COM")
+            Btnconectar.Enabled = True
+        Else
+            MessageBox.Show("no hay puertos diponibles")
+            Cbopuertos.Items.Clear()
+
+        End If
+    End Sub
+
+    Private Sub Btnconectar_Click(sender As Object, e As EventArgs) Handles Btnconectar.Click
+        If Btnconectar.Text = "CONECTAR" Then
+
+            Try
+                With PuertoCom
+                    .BaudRate = 57600
+                    .DataBits = 8
+                    .Parity = IO.Ports.Parity.None
+                    .StopBits = IO.Ports.StopBits.One
+                    .PortName = Cbopuertos.Text
+                    .Open()
+                End With
+                Btnconectar.Text = "DESCONECTAR"
+                Btnevniardatos.Enabled = True
+
+            Catch ex As Exception
+                MsgBox(ex.Message, MsgBoxStyle.Critical)
+
+            End Try
+        ElseIf Btnconectar.Text = "DESCONECTAR" Then
+
+            Btnconectar.Text = "CONECTAR"
+            Btnevniardatos.Enabled = False
+            PuertoCom.Close()
+
+        End If
+    End Sub
+
+
+
+    Private Sub Btnevniardatos_Click(sender As Object, e As EventArgs) Handles Btnevniardatos.Click
+        Dim AuxLargoBobina As Decimal
+        Dim AuxCambiarSentido As String
+
+
+
+        'Dim AuxlargoBobina As Integer = CDbl(Me.TxtLargoBobina.Text)'
+
+
+        AuxLargoBobina = Convert.ToDecimal(TxtLargoBobina.Text)
+        'AuxLargoBobina = Integer.Parse(TxtLargoBobina.Text)'
+
+
+        LoopCambiarSentido = AuxLargoBobina / DiametroDeCable
+        AuxCambiarSentido = LoopCambiarSentido.ToString()
+
+
+        enviando = True 'indica que se envian datos (decarta el buffer de entrada)
+
+
+        StrBufferSalida = ""
+        StrBufferSalida = "#" & TxtVueltas.Text & "_" & PasoxVuelta & "_" & AuxCambiarSentido & "_" & TipoPaso & "_" & estado & "_% "
+
+
+        ' StrBufferSalida = "#" & TxtVueltas.Text & "_" & TxtLargoBobina.Text & "_" & Cbcalibre.SelectedItem & "_" & estado & "_%"'
+        'PuertoCom.Write(StrBufferSalida)
+        PuertoCom.Write(StrBufferSalida)
+
+
+
+
+    End Sub
+
+
+    'esto es una interrupcion que ocurre cuando recibe datos seriales
+    Private Sub PuertoCom_DataReceived(sender As Object, e As IO.Ports.SerialDataReceivedEventArgs) Handles PuertoCom.DataReceived
+        Dim DatoInterrupcion As String
+        DatoInterrupcion = PuertoCom.ReadLine
+
+        PuertaAccesoInterrupcion(DatoInterrupcion)
+
+
+    End Sub
+
+
+
+
+
+    Private Sub Cbcalibre_SelectedIndexChanged(sender As Object, e As EventArgs) Handles Cbcalibre.SelectedIndexChanged
+
+
+        Select Case Cbcalibre.SelectedIndex
+            Case 0 'calibre 26AWG'
+
+                PasoxVuelta = "10" 'Pasos de 0.04 por Vuelta'
+                TipoPaso = "1"
+                DiametroDeCable = 0.4
+
+            Case 1
+                PasoxVuelta = "10"
+                TipoPaso = "1"
+                DiametroDeCable = 0.4
+            Case 2
+                PasoxVuelta = "10"
+                TipoPaso = "1"
+                DiametroDeCable = 0.4
+
+        End Select
+
+
+    End Sub
+
+    Private Sub AyudaToolStripMenuItem2_Click(sender As Object, e As EventArgs)
+        Try
+            System.Diagnostics.Process.Start("https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley")
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Ayuda2ToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles Ayuda2ToolStripMenuItem.Click
+        Try
+            System.Diagnostics.Process.Start("https://github.com/Makiben/Ayuda-Bobinadora/blob/main/Ayuda%20Bobina")
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub TxtVueltas_TextChanged(sender As Object, e As EventArgs) Handles TxtVueltas.TextChanged
+        If IsNumeric(TxtVueltas.Text) = False Then
+            TxtVueltas.Text = ""
+
+        End If
+
+        If TxtVueltas.Text = "" And TxtLargoBobina.Text = "" Then
+            Btnevniardatos.Enabled = False
+
+        ElseIf TxtVueltas.Text = "" And Not TxtLargoBobina.Text = "" Then
+            Btnevniardatos.Enabled = False
+
+        ElseIf Not TxtVueltas.Text = "" And TxtLargoBobina.Text = "" Then
+            Btnevniardatos.Enabled = False
+
+        Else
+            Btnevniardatos.Enabled = True
+
+        End If
+
+
+    End Sub
+
+    Private Sub TxtLargoBobina_TextChanged(sender As Object, e As EventArgs) Handles TxtLargoBobina.TextChanged
+        If IsNumeric(TxtLargoBobina.Text) = False Then
+            TxtLargoBobina.Text = ""
+
+        End If
+
+        If TxtVueltas.Text = "" And TxtLargoBobina.Text = "" Then
+            Btnevniardatos.Enabled = False
+
+        ElseIf TxtVueltas.Text = "" And Not TxtLargoBobina.Text = "" Then
+            Btnevniardatos.Enabled = False
+
+        ElseIf Not TxtVueltas.Text = "" And TxtLargoBobina.Text = "" Then
+            Btnevniardatos.Enabled = False
+
+        Else
+            Btnevniardatos.Enabled = True
+
+        End If
+    End Sub
+End Class
+
+
+
+' concatenar  tramas VB´'
+
+'ingresar solo numeros L, cuando pones enviar datos verificar que todo este correcto,que los campos no esten vacios , desabilitar el boton enviar hasta que los textbox esten llenos L
+'truncar los decimales 
+'"#" & TxtVueltas.Text & "_" & TxtLongitud.Tex & "_" & TxtTxtCalibre'
